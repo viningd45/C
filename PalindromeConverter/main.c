f5345e3a7d16d35201d1e4dc1d8216a5f70674fa
Daniel Vining <viningd45@lsus.edu>
2018-07-18 21:08:51 -0500

    Get user input and retunr to user

--- PalindromeConverter/main.c
+++ PalindromeConverter/main.c
@@ -1,5 +1,15 @@
 
+#include <stdio.h>
+
+#define WORD_LENGTH 50
+
 int main() 
 {
+	char userInput[WORD_LENGTH];
+	
+	printf("Test to see if a word is a palindrome!\nEnter a word below...\n");
+	scanf("%s", userInput);
+	
+	printf("The word you entered is: %s", userInput);
 	return 0;
 }	
